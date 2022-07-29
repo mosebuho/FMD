d66ec6c99cbcf8765c9e008dc948e8519465f781
mosebuho <1198kguz@gmail.com>
2022-07-30 02:23:20 +0900

    유저 모델 재정의

--- user/models.py
+++ user/models.py
@@ -1,12 +1,52 @@
-from django.contrib.auth.models import AbstractUser
+from django.db import models
+from django.contrib.auth.models import BaseUserManager, AbstractBaseUser
 
-app_name = "user"
+
+class UserManager(BaseUserManager):
+    def create_user(self, email, password=None):
+        if not email:
+            raise ValueError("Users must have an email address")
+
+        user = self.model(
+            email=self.normalize_email(email),
+        )
+
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, email, password):
+        user = self.create_user(
+            email,
+            password=password,
+        )
+        user.is_admin = True
+        user.save(using=self._db)
+        return user
 
 
-class User(AbstractUser):
-    pass
+class User(AbstractBaseUser):
+    email = models.EmailField(max_length=128, unique=True, verbose_name="이메일")
+    name = models.CharField(max_length=60, unique=True, verbose_name="닉네임")
+    join_date = models.DateTimeField(auto_now_add=True, verbose_name="생성시간")
+    point = models.PositiveIntegerField(default=0, verbose_name="포인트")
+    is_active = models.BooleanField(default=True)
+    is_admin = models.BooleanField(default=False)
 
-    class Meta:
-        db_table = "User"
-        verbose_name = "유저"
-        verbose_name_plural = "유저"
+    objects = UserManager()
+
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = []
+
+    def __str__(self):
+        return self.email
+
+    def has_perm(self, perm, obj=None):
+        return True
+
+    def has_module_perms(self, app_label):
+        return True
+
+    @property
+    def is_staff(self):
+        return self.is_admin
