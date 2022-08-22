31aba1c5f9d49dcc3728b3ad4a740f3c7ecc3aa9
mosebuho <1198kguz@gmail.com>
2022-08-22 13:48:28 +0900

    홈 랭킹 생성 & 게시글 글자수 제한

--- FMD/views.py
+++ FMD/views.py
@@ -1,11 +1,14 @@
 from django.views import generic
 from board.models import Community
+from user.models import User
+
 
 class HomeView(generic.TemplateView):
     template_name = "home/home.html"
-    
+
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        context["commu_list_free"] = Community.objects.filter(name="free")[:14]
-        context["commu_list_info"] = Community.objects.filter(name="info")[:14]
+        context["commu_list_free"] = Community.objects.filter(name="자유")[:14]
+        context["commu_list_info"] = Community.objects.filter(name="정보")[:14]
+        context["ranking"] = User.objects.order_by("-point")[:5]
         return context
