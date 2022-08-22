44447c0dec7d03bd93d38283950650728ea99159
mosebuho <1198kguz@gmail.com>
2022-08-22 16:55:17 +0900

    추천 비로그인 알림, media 파일 git 제외

--- board/models.py
+++ board/models.py
@@ -4,6 +4,8 @@ from django.utils import timezone
 from user.models import User
 
 name_choice = (("자유", "자유"), ("정보", "정보"))
+
+
 class Community(models.Model):
     title = models.CharField(max_length=64, verbose_name="제목")
     content = models.TextField(verbose_name="내용")
@@ -29,3 +31,22 @@ class Community(models.Model):
         db_table = "Community"
         verbose_name = "커뮤니티"
         verbose_name_plural = "커뮤니티"
+
+
+class Comment(models.Model):
+    community = models.ForeignKey(
+        Community, null=True, blank=True, on_delete=models.CASCADE, verbose_name="게시글"
+    )
+    writer = models.ForeignKey(
+        User, on_delete=models.SET_NULL, null=True, verbose_name="작성자"
+    )
+    content = models.CharField(max_length=2000, verbose_name="댓글 내용")
+    date = models.DateTimeField(auto_now_add=True, verbose_name="작성일")
+
+    @property
+    def date_str(self):
+        time = timezone.now() - self.date
+        if time < timedelta(days=1):
+            return self.date.strftime("%H:%M")
+        else:
+            return self.date.strftime("%m-%d")
