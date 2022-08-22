117e82cf698b9b8ce20388f41c3135348515ddd3
mosebuho <1198kguz@gmail.com>
2022-08-22 13:33:21 +0900

    게시판글 홈으로 불러오기

--- FMD/views.py
+++ FMD/views.py
@@ -1,4 +1,11 @@
 from django.views import generic
+from board.models import Community
 
 class HomeView(generic.TemplateView):
     template_name = "home/home.html"
+    
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["commu_list_free"] = Community.objects.filter(name="free")[:14]
+        context["commu_list_info"] = Community.objects.filter(name="info")[:14]
+        return context
