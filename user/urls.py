921a9321b897f0731a518419f9095408304700fc
mosebuho <1198kguz@gmail.com>
2022-09-01 08:32:11 +0900

    내정보 페이지 구현(info 페이지 미완성) & CSS

--- user/urls.py
+++ user/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path
 from .views import RegisterView
 from django.contrib.auth.views import LoginView, LogoutView
+from . import views
 
 app_name = "user"
 urlpatterns = [
     path("register/", RegisterView.as_view(), name="register"),
     path("login/", LoginView.as_view(), name="login"),
     path("logout/", LogoutView.as_view(), name="logout"),
+    path("profile/<int:pk>/", views.ProfileView.as_view(), name="profile"),
 ]
--- user/views.py
+++ user/views.py
@@ -1,9 +1,18 @@
 from django.views import generic
 from .forms import RegisterForm
 from django.urls import reverse
+from .models import User
+from django.shortcuts import get_object_or_404, render
+
 
 class RegisterView(generic.CreateView):
     template_name = "user/register.html"
     form_class = RegisterForm
+
     def get_success_url(self):
         return reverse("user:login")
+
+
+class ProfileView(generic.TemplateView):
+    template_name = "user/profile.html"
+    model = User
