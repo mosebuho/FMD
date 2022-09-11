408c0d7374edf109b6960b694fe37144cff29bac
mosebuho <1198kguz@gmail.com>
2022-09-12 08:31:41 +0900

    summernote 추가 CSS & 뉴스 생성 버튼

--- board/forms.py
+++ board/forms.py
@@ -6,10 +6,12 @@ class CommuModelForm(forms.ModelForm):
     class Meta:
         model = Community
         fields = ["title", "name", "content"]
+        widgets = {'title': forms.TextInput(attrs={'placeholder': '제목을 입력해주세요.'}),}
 
 
 class NewsModelForm(forms.ModelForm):
     class Meta:
         model = News
         fields = ["title", "thumbnail", "name", "content"]
+        widgets = {'title': forms.TextInput(attrs={'placeholder': '제목을 입력해주세요.'}),}
     
