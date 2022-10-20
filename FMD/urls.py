7ccb6349d01d5e41e2de0ae4d9f22a5f86985aae
mosebuho <1198kguz@gmail.com>
2022-10-20 13:49:53 +0900

    통합검색 기능 추가

--- FMD/urls.py
+++ FMD/urls.py
@@ -1,12 +1,13 @@
 from django.contrib import admin
 from django.urls import path, include
-from .views import HomeView
+from .views import HomeView, search
 from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("", HomeView.as_view()),
+    path("search/", search, name="search"),
     path("user/", include("user.urls")),
     path("board/", include("board.urls")),
 ]
--- FMD/views.py
+++ FMD/views.py
@@ -1,5 +1,6 @@
+from django.shortcuts import render
 from django.views import generic
-from board.models import Community, News, Column
+from board.models import Community, News, Column, Event
 
 
 class HomeView(generic.TemplateView):
@@ -9,8 +10,35 @@ class HomeView(generic.TemplateView):
         context = super().get_context_data(**kwargs)
         context["commu_free_new"] = Community.objects.filter(name="자유")[:14]
         context["commu_info_new"] = Community.objects.filter(name="정보")[:14]
-        context["commu_free_hot"] = Community.objects.filter(name="자유").order_by("-like")[:14]
-        context["commu_info_hot"] = Community.objects.filter(name="정보").order_by("-like")[:14]
+        context["commu_free_hot"] = Community.objects.filter(name="자유").order_by(
+            "-like"
+        )[:14]
+        context["commu_info_hot"] = Community.objects.filter(name="정보").order_by(
+            "-like"
+        )[:14]
         context["news_list"] = News.objects.order_by("-id")[:3]
         context["column_list"] = Column.objects.order_by("-id")[:6]
         return context
+
+
+def search(request):
+    community = Community.objects.all().order_by("-id")
+    news = News.objects.all().order_by("-id")
+    column = Column.objects.all().order_by("-id")
+    event = Event.objects.all().order_by("-id")
+
+    q = request.POST.get("q", "")
+    context = {"q": q}
+
+    if q:
+        community = community.filter(title__icontains=q)
+        news = news.filter(title__icontains=q)
+        column = column.filter(title__icontains=q)
+        event = event.filter(title__icontains=q)
+        context = {
+            "community": community,
+            "news": news,
+            "column": column,
+            "event": event,
+        }
+    return render(request, "home/search.html", context)
