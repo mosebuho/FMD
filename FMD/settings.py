a634d56492fd6afb8dc2c0b3d6d1d0cda883e39b
mosebuho <1198kguz@gmail.com>
2022-11-19 14:40:35 +0900

    static settings 수정 아오

--- FMD/settings.py
+++ FMD/settings.py
@@ -86,13 +86,7 @@ USE_I18N = True
 USE_TZ = False
 
 STATIC_URL = "static/"
-
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "static"),
-]
-
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-
 
 MEDIA_URL = "/media/"
 MEDIA_ROOT = os.path.join(BASE_DIR, "media/")
