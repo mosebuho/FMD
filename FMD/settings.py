df8f39b3f52b48cd859bc73489aa7982e0838026
mosebuho <1198kguz@gmail.com>
2022-11-19 14:35:59 +0900

    static 등록

--- FMD/settings.py
+++ FMD/settings.py
@@ -91,6 +91,9 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static"),
 ]
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
+
 MEDIA_URL = "/media/"
 MEDIA_ROOT = os.path.join(BASE_DIR, "media/")
 
